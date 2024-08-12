fa6ea8505f9bb557595081bffbaebd02dc73ec4c
Andy Tam <andytam321@gmail.com>
2024-08-12 22:58:03 +0800

    added force buy/sell all if all cards are revealed

--- players/Andy.py
+++ players/Andy.py
@@ -32,6 +32,16 @@ class Andy(Player):
             return statistics.mean(map(sum, combinations(self.cards, self.card_count - n)))
         
     def decide(self, visible_cards, buy_price, sell_price):
+        n = len(visible_cards)
+        if n == self.card_count:
+            e = sum(visible_cards)
+            if e > buy_price:
+                return (True, self.max_number(buy_price) - 1)
+            elif e < sell_price:
+                return (False, self.max_number(sell_price) - 1)
+            else:
+                return (True, 0)
+
         if tuple(visible_cards) in self.e_cache:
             e = self.e_cache[tuple(visible_cards)]
         else:
@@ -41,7 +51,6 @@ class Andy(Player):
             # print(f"Buy: {buy_price}")
             # print(f"Sell: {sell_price}")
 
-            n = len(visible_cards)
             for i in visible_cards:
                 self.cards.remove(i)
             e = self.expected_value(n) + sum(visible_cards)
@@ -67,5 +76,5 @@ class Andy(Player):
 def buildAndy(card_count, round, budget):
     # calculate optimal value of alpha
     alpha = min(0.7, ((round - 50) / 200 * 0.4 + 0.3))
-    return Andy("Andy", card_count, round, budget, tolerance=0.01, alpha=alpha, estimate_expected=card_count > 3)
+    return Andy("Andy", card_count, round, budget, tolerance=0.01, alpha=alpha, estimate_expected=card_count>3)
 
